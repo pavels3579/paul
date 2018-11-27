c230f10a02fd432678282a1ce1542c6366ff8d43
pavels3579 <pavels357@yandex.ru>
2018-11-28 00:23:25 +0400

    eigth task

---
+++ lesson 8/cargo_carriage.rb
@@ -0,0 +1,18 @@
+class CargoCarriage < Carriage
+  attr_reader :number, :volume
+  attr_accessor :taken_volume
+
+  def initialize(number, volume)
+    @number = number
+    @volume = volume
+    @taken_volume ||= 0
+    validate!
+    validate_cargo!
+  end
+
+  private
+
+  def validate_cargo!
+    raise 'Не указано объем' if @volume.zero?
+  end
+end
