f06f087a0f7c49cebbe11e3f364ba512baf50e4a
pavels3579 <pavels357@yandex.ru>
2018-12-01 00:06:35 +0400

    update task nine

--- lesson 9/accessors.rb
+++ lesson 9/accessors.rb
@@ -1,42 +1,38 @@
 module Accessors
-  @history ||= []
-
-  def attr_accessor_with_history(*names)
-    names.each do |name|
-      var_name = "@#{name}".to_sym
-      define_method(name) { instance_variable_get(var_name) }
-      define_method("#{name}=".to_sym) { |value| instance_variable_set(var_name, value) }
-      @history << { |value| }
-    end
+  def self.included(base)
+    base.extend ClassMethods
   end
 
-  def strong_attr_accessor(attr_name, attr_class)
-    var_name = "@#{attr_name}".to_sym
-    define_method(attr_name) { instance_variable_get(var_name) }
-    if { |value| value.is_a? attr_class}
-      define_method("#{attr_name}=".to_sym) { |value| instance_variable_set(var_name, value) }
-    else
-      raise "It's not the same type as attr_class"
+  module ClassMethods
+
+    def attr_accessor_with_history(*names)
+      names.each do |name|
+        var_name = "@#{name}".to_sym
+        name_history = "@#{name}_history".to_sym
+
+        instance_variable_set(name_history, []) if instance_variable_get(name_history),nil?
+
+        define_method(name) { instance_variable_get(var_name) }
+        define_method("#{name}=".to_sym) do |value|
+          instance_variable_set(var_name, value)
+          instance_variable_get(name_history) << value
+        end
+      end
+    end
+
+    def strong_attr_accessor(attr_name, attr_class)
+      var_name = "@#{attr_name}".to_sym
+      define_method(attr_name) { instance_variable_get(var_name) }
+      if { |value| value.is_a? attr_class}
+        define_method("#{attr_name}=".to_sym) { |value| instance_variable_set(var_name, value) }
+      else
+        raise "It's not the same type as attr_class"
+      end
+    end
+
+    define_method("#{name}_history") do
+      instance_variable_get("#{name}_history".to_sym)
     end
   end
 
 end
-
-=begin
-module MyAttrAccessor
-
-  def my_attr_accessor(*names)
-    names.each do |name|
-      var_name = "@#{name}".to_sym
-      define_method(name) { instance_variable_get(var_name) }
-      define_method("#{name}=".to_sym) { |value| instance_variable_set(var_name, value) }
-    end
-  end
-end
-
-class Test
-  extend attr_accessor_with_history
-
-  my_attr_accessor :my_attr, :a, :b, :c
-end
-=end
--- lesson 9/validation.rb
+++ lesson 9/validation.rb
@@ -4,4 +4,30 @@ module Validation
   rescue RuntimeError
     false
   end
+
+  class Validate
+
+    def valid_presence(name)
+      !name.nil && !name.empty? || raise "it's can't be nil or empty"
+    end
+
+    def valid_format(name,format)
+      mane =~ format || raise "it's not format needed"
+    end
+
+    def valid_type(name,class_type)
+      name.class == class_type || raise "it's not the same class type"
+    end
+
+    def validate(name. format = nil, class_type = nil)
+      if fopmat.nil? && class_type_nil?
+        valid_presence(name)
+      elsif !format.nil?
+        valid_format(name,format)
+      elsif !class_type_nil?
+        valid_type(name,class_type)
+      end
+    end
+
+  end
 end
