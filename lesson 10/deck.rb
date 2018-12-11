fc7783e529f024d1c6c20c1cc28a8d2b3212f93a
pavels3579 <pavels357@yandex.ru>
2018-12-11 20:26:33 +0400

    8 update task ten

--- lesson 10/deck.rb
+++ lesson 10/deck.rb
@@ -2,13 +2,11 @@ class Deck
   attr_reader :cards
 
   def initialize
-    @cards = []
-    Card::SUITS.each do |suit|
+    @cards = Card::SUITS.each.with_object([]) do |suit, cards|
       Card::CARDS_POINTS.each do |name_card, points|
         other_points = Card::CARDS_OTHER_POINTS[name_card]
         card = Card.new(suit, name_card, points, other_points)
-        @cards << card
       end
-    end
+    end.shuffle!
   end
 end
